0cee1ed402a92e18534b695522e1a74593887728
Marco Valdez <valdez.rodea.marcoantonio@gmail.com>
2024-02-19 16:39:36 -0600

    CRUD heroes implemented

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,14 +1,125 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+
+import { filter, switchMap, tap } from 'rxjs';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'heroes-new-page',
   templateUrl: './new-page.component.html',
-  styles: ``
+  styleUrls:['./new-page.component.css']
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit{
+
+
+  public heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
 
   public publishers = [
-    { id:'DC Comics', desc: 'DC - Comics' },
-    { id:'Marvel Comics', desc: 'Marvel - Comics' },
+    { id: 'DC Comics', desc: 'DC - Comics' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
+
+  public action:"Actualizar" | "Guardar" = "Guardar";
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router:Router,
+    private snackbar:MatSnackBar,
+    private dialog: MatDialog,
+  ) { }
+
+  ngOnInit(): void {
+    if(!this.router.url.includes('edit')) return;
+    this.activatedRoute.params
+    .pipe(
+      switchMap(({id})=> this.heroesService.getHeroById(id))
+    ).subscribe(hero=>{
+      if(!hero) return this.router.navigateByUrl('/');
+      this.heroForm.reset(hero);
+      return;
+    })
+
+    this.selectAction()
+  }
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+  onSubmit(): void {
+    if (this.heroForm.invalid) return;
+
+    if (this.currentHero.id) {
+      this.heroesService.updateHero(this.currentHero)
+        .subscribe(hero => {
+          // TODO: mostrar snackbar
+          this.showSnackbar(`${hero.superhero} succesfuly updated`)
+        });
+      return;
+    }
+
+    this.heroesService.addHero(this.currentHero)
+      .subscribe(hero => {
+        // TODO: mostrar snackbar y navegar a heroes/edit/hero.id
+        this.router.navigate(['heroes/edit',hero.id])
+        this.showSnackbar(`${hero.superhero} succesfuly created`)
+      });
+  }
+
+  onDelteHero(){
+    if (!this.currentHero.id) throw Error('Hero id is required');
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: this.heroForm.value
+    });
+
+    dialogRef.afterClosed()
+    .pipe(
+      filter((result:boolean)=>result),
+      switchMap(()=>this.heroesService.deleteHeroById(this.currentHero.id)),
+      filter((wasDeleted:boolean) => wasDeleted),
+    )
+    .subscribe( ()=>{
+      this.router.navigate(['/heroes'])
+    })
+
+    // primera forma en que se habia hecho, no es incorrecta
+    // pero es dificil leer un suscribe dentro de otro
+    // dialogRef.afterClosed().subscribe(result => {
+    //   if(!result) return;
+
+    //   this.heroesService.deleteHeroById(this.currentHero.id)
+    //   .subscribe( wasDeleted => {
+    //     if(wasDeleted)
+    //       this.router.navigate(['/heroes'])
+    //   });
+    // });
+  }
+
+  showSnackbar(message: string):void{
+    this.snackbar.open(message, 'DONE',{
+      duration: 1500,
+    })
+  }
+
+  selectAction():void{
+    this.action = (!this.currentHero.id) ? "Actualizar" : "Guardar";
+    console.log(this.action);
+
+  }
 }
